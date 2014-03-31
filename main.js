d213870ee9efef94ae49819e1890f7dc184caedb
Brian Cavalier <brian@hovercraftstudios.com>
2014-03-31 08:45:48 -0400

    Initial port of briancavalier/slides to use RaveJS

---
+++ main.js
@@ -0,0 +1,27 @@
+var fluent = require('wire/config/fluent');
+var merge = require('wire/config/merge');
+var dom = require('wire/dom');
+
+var SlideModel = require('./lib/SlideModel');
+var SlideView = require('./lib/SlideView');
+var PresentationController = require('./lib/PresentationController');
+var split = require('./lib/slides/split');
+var markdown = require('./lib/slides/markdown');
+
+module.exports = merge([dom, fluent(function(context) {
+	return context
+		.add('slides', function() {
+			return require.async('./slides.txt')
+				.then(markdown())
+				.then(split(/\s*\<hr\s*\/?\>\s*/i));
+		})
+		.add('slidesNode', ['qs'], function(qs) {
+			return qs('body');
+		})
+		.add('model', ['slides'], SlideModel)
+		.add('view', ['slidesNode', 'model'], SlideView)
+		.add('controller', ['view'], PresentationController)
+		.add('@startup', ['controller'], function(controller) {
+			return controller.start();
+		});
+})]);
